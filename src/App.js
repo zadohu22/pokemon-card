1118926a7ec0ed6d5689840416e5daf8b62afdf9
zadohu22 <104797988+zadohu22@users.noreply.github.com>
2023-04-06 18:36:50 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='w-full h-full flex justify-center items-center'>
+      <h1 className='text-3xl text-green-600'>Hello world.</h1>
+    </div>
+  );
+}
+
+export default App;
